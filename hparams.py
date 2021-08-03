a16d8b7657fd1b8ee1728b68ec9a4509c696a3f4
Val <valkenneth.arado@yahoo.com>
2021-08-04 01:52:07 +0800

    hparams

--- hparams.py
+++ hparams.py
@@ -9,8 +9,8 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Experiment Parameters        #
         ################################
-        epochs=500,
-        iters_per_checkpoint=1000,
+        epochs=1000,
+        iters_per_checkpoint=5000,
         seed=1234,
         dynamic_loss_scaling=True,
         fp16_run=False,
@@ -25,8 +25,8 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
+        training_files='filelists/train.txt',
+        validation_files='filelists/test.txt',
         text_cleaners=['english_cleaners'],
 
         ################################
